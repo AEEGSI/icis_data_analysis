8b3c66ccf1bbb4a21a85912cdfb3a661d76a8960
Iwan Buetti <iwan.buetti@gmail.com>
2017-10-19 15:07:14 +0200

    bug fix

--- lib/icis_data_analysis/array.rb
+++ lib/icis_data_analysis/array.rb
@@ -1,6 +1,6 @@
 class Array
   def average(attribute=nil)
-    return nil if size==0.0
+    return nil if empty?
     inject(0.0){ |sum, el| sum + (attribute.nil? ? el : el.send(attribute)) }.to_f / size
   end
 end
